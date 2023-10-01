2efd259399c2b91ee19d12d411346771a76cdaa7
Pavel Okhrim <paokhrim@gmail.com>
2023-10-01 12:42:40 +0300

    Исправления после ревью.

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -76,12 +76,15 @@ class Follow(models.Model):
     )
 
     class Meta:
-        constraints = [
+        constraints = (
             models.UniqueConstraint(
-                fields=['user', 'following'],
+                fields=('user', 'following'),
                 name='unique_follow'
-            )
-        ]
+            ),
+        )
 
     def __str__(self):
-        return f"{self.user} подписан на {self.following}"
+        return (
+            f'{self.user.username} подписан на '
+            f'{self.following.username}'
+        )
